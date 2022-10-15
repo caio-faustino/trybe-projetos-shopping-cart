9853f49fa981bc69abaeeadaee80fc88aceebd42
caio-faustino <caioexfaustino.pro@gmail.com>
2022-10-15 18:23:09 -0300

    requisite 03

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
+const fetchItem = async (search) => {
   // seu código aqui
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${search}`;
+    const reply = await fetch(url);
+    const data = await reply.json();
+    return data;
+  } catch (error) {
+    throw new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,17 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('its a function', () => {
+    expect(typeof fetchItem).toBe('function')
+  })
+  it('call MLB1615760527', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  })
+  it('endpoint', async () => {
+    await fetchItem('MLB1615760527')
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('provide url', async () => {
+    await expect(() => fetchItem()).rejects.toThrow('You must provide an url');
+  })
 });
