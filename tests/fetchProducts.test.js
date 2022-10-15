d2c03b94fff18fa9687b033b0b0f2d15f679f2f1
caio-faustino <caioexfaustino.pro@gmail.com>
2022-10-15 17:07:31 -0300

    requisite 01

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,19 @@
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
-
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('its a function', () => {
+    expect(typeof fetchProducts).toBe('function')
+  })
+  it('call computador', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  })
+  it('endpoint', async () => {
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('provide url', async () => {
+    await expect(() => fetchProducts()).rejects.toThrow('You must provide an url');
+  })
 });
